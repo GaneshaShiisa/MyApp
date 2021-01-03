937e3b63c4734c92bbf77093b09a7e9288694487
Ganesha Shiisa <ganesha.shiisa@gmail.com>
2021-01-03 02:13:16 +0000

    real-time-clock compoent を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RealTimeClockComponent } from './real-time-clock/real-time-clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RealTimeClockComponent
   ],
   imports: [
     BrowserModule,
